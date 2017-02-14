7c27b280524f1c53bb26b5860a0b1377e4b31700
hovanquang <vanquangtpm@gmail.com>
2017-02-14 08:20:54 +0700

    change some file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :microposts
   resources :users
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get  'static_pages/about'
   root 'users#index'
 end
